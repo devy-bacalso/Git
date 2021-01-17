ed99fc06d5e7dd672cb146cfcee835c4e4e17080
Devy <dbacalso@gmail.com>
2021-01-17 12:28:44 +0000

    root commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1611486001;s:7:"authKey";s:64:"Q;%8J-M}6bHT!z%BfhB[]`5aNPfIE}R9=k=koK.*`0myq`T({$OORM7A/,S*B^ln";}
